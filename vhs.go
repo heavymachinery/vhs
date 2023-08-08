c27a4fded9de8bc9ef0888387c7439734ec8abd1
Dylan Geraci <67774922+heavymachinery@users.noreply.github.com>
2023-08-08 06:12:58 +0000

    remove extra linebreak

--- vhs.go
+++ vhs.go
@@ -118,7 +118,6 @@ func (vhs *VHS) Start() error {
 	}
 
 	port := randomPort()
-
 	vhs.tty = buildTtyCmd(port, vhs.Options.Shell, vhs.Options.Prompt)
 	if err := vhs.tty.Start(); err != nil {
 		return fmt.Errorf("could not start tty: %w", err)
